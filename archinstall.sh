f86428941719f88e970f8d2b2384a56de0f0ffc2
am-zak <merzak_aoudia@hotmail.com>
2021-05-29 10:39:11 +0200

    colors

--- archinstall.sh
+++ archinstall.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 clear
-echo "My arch installer!"
+echo "\033[32mMy arch installer!\033[0m"
 pacman --noconfirm -Sy archlinux-keyring
 loadkeys fr
 timedatectl set-ntp true
 clear && lsblk
-echo "Enter device name (full path) : "
+echo "\033[32mEnter device name (full path) : \033[0m"
 read device
 fdisk $device
-echo "Now! Mount and make file systems on partitions manually"
+echo "\033[32mNow! Mount and make file systems on partitions manually\033[0m"
 sed '1,/^#install$/d' $0 > archinstall_2.sh
 chmod +x archinstall_2.sh
 exit
 
 #install
 clear
-echo "Mount point of system partition (full path):"
+echo "\033[32mMount point of system partition (full path):\033[0m"
 read syspart
 pacstrap $syspart base base-devel linux linux-firmware grub vim git stow
 genfstab -U $syspart >> $syspart/etc/fstab
@@ -42,18 +42,19 @@ mkinitcpio -P
 passwd
 clear
 lsblk
-echo "Installing grub on, device (full path) : "
+echo "\033[32mInstalling grub on, device (full path) : \033[0m"
 read device
 grub-install $device
 grub-mkconfig -o /boot/grub/grub.cfg
-echo "Installing some necessary programs ... "
+echo "\033[32mInstalling some necessary programs ... \033[0m"
 pacman -S --noconfirm pulseaudio pulseaudio-alsa alsa-utils networkmanager firefox pamixer vim vi
 systemctl enable NetworkManager
-echo "Username? : "
+systemctl start NetworkManager
+echo -e "\033[32mUsername? : \033[0m"
 read user
 useradd -m -G wheel -s /bin/bash $user
 passwd $user
 visudo
 su -c 'cd ~ && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg --noconfirm -si PKGBUILD' $user
-echo "Done!"
-echo "Reboot now"
+echo "\033[33mDone!"
+echo "Reboot now\033[0m"
